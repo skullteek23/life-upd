4ef75083f2d9aa62f8a8389764aadd249094ec0d
skullteek23 <pgoel681@gmail.com>
2026-09-26 00:00:35 +0530

    setup postgres

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import 'dotenv/config';
 import app from "./app"
+import pool from './db/postgres';
 
 const PORT = Number(process.env.PORT) || 2000;
 
-app.listen(PORT, () => {
-    console.log('server running')
+app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+
+    try {
+        await pool.connect();
+        console.log('PostgreSQL connected');
+    } catch (error) {
+        console.log('PostgreSQL connection failed: ', error);
+    }
 })
